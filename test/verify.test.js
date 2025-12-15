69bb8c6fc8153f8df56e8e605f70d5dcf664bcb8
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-12-15 13:17:16 -0600

    Update verify.test.js

--- test/verify.test.js
+++ test/verify.test.js
@@ -28,7 +28,7 @@ afterAll(() => {
 });
 
 beforeEach(async () => {
-  browser = await puppeteer.launch();
+  browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   page = await browser.newPage();
   await page.goto("http://localhost:3000/index.html");
 });
